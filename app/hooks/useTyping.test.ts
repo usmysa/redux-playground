211cc8b7a0cfae1ef9dfa8a0795636b867eee493
usmysa <usmysa@gmail.com>
2024-03-30 16:32:33 +0900

    alter bun:test to npm

--- app/hooks/useTyping.test.ts
+++ app/hooks/useTyping.test.ts
@@ -1,9 +1,12 @@
-import { describe, expect, it, mock } from "bun:test";
+// import { describe, expect, it, mock } from "bun:test";
 import { generateWord } from "./useTyping";
 
 describe("generateWord", () => {
   it("should return correct values", () => {
-    mock.module("random-words", () => ({ generate: () => "mocked" }));
+    vi.mock("random-words", () => ({
+      generate: vi.fn().mockReturnValue("mocked"),
+    }));
+    // mock.module("random-words", () => ({ generate: () => "mocked" }));
     const word = generateWord(3, 5);
 
     expect(word).toMatchObject(["m", "o", "c", "k", "e", "d"]);
