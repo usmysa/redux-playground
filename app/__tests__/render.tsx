79238d6c136b23dec2bbe40d5a668710665c4d86
usmysa <usmysa@gmail.com>
2024-03-31 01:59:47 +0900

    use render of react-testing

--- app/__tests__/render.tsx
+++ app/__tests__/render.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from "@/providers/theme";
 import { store } from "@/store";
+import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 
 export const renderWithProviders = (ui: React.ReactElement) => {
-  return (
+  return render(
     <ThemeProvider>
       <Provider store={store}>{ui}</Provider>
-    </ThemeProvider>
+    </ThemeProvider>,
   );
 };
